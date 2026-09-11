3038e20871d017abdf1867fdfbb18e1f742f9fd2
David Stopschinski <stopschinski.teamschnitzler@gmail.com>
2026-09-11 14:32:28 +0200

    Move prototype tracks to isolated track lab

---
+++ assets/tracks/track-lab-loader.js
@@ -0,0 +1,8 @@
+/* Register prototype tracks only when the dedicated Track-Lab URL requests them. */
+if (new URLSearchParams(location.search).get('trackLab') === '1') {
+  for (const src of [
+    'assets/tracks/shanghai_2018.js?v=20260911b',
+    'assets/tracks/madring_2026.js?v=20260911b',
+    'assets/tracks/spa_francorchamps_2022.js?v=20260911b'
+  ]) document.write('<script src="' + src + '" defer><' + '/script>');
+}
